fdd6a013adf1eb0bfe5beaa8efaaba545f904534
mdsadatrahman3 <mdsadatrahman03@gmail.com>
2026-06-25 23:57:24 +0600

    fix: remove 401 redirect interceptor (PrivateRoute handles auth guards)

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -16,14 +16,4 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      window.location.href = '/login';
-    }
-    return Promise.reject(error);
-  }
-);
-
 export default api;
